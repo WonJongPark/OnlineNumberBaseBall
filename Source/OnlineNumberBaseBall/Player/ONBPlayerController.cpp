d37a1c9cf9d6650759b1d573b9773b9eabf6dc65
Park WonJong <kidsmon@naver.com>
2026-03-15 00:46:41 +0900

    feat: PlayerController에서 시작시 위젯 생성 및 입력함수 바인딩

--- Source/OnlineNumberBaseBall/Player/ONBPlayerController.cpp
+++ Source/OnlineNumberBaseBall/Player/ONBPlayerController.cpp
@@ -2,3 +2,34 @@
 
 
 #include "ONBPlayerController.h"
+
+#include "UI/ONBChatInput.h"
+
+void AONBPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	FInputModeUIOnly InputModeUIOnly;
+	SetInputMode(InputModeUIOnly);
+	
+	if (IsValid(ChatInputWidgetClass))
+	{
+		ChatInputWidgetInstance = CreateWidget<UONBChatInput>(this, ChatInputWidgetClass);
+		if (IsValid(ChatInputWidgetInstance))
+		{
+			ChatInputWidgetInstance->AddToViewport();
+		}
+	}
+}
+
+void AONBPlayerController::SetChatMessageString(const FString& IntChatMessageString)
+{
+	ChatMessageString = IntChatMessageString;
+	
+	PrintChatMessageString(ChatMessageString);
+}
+
+void AONBPlayerController::PrintChatMessageString(const FString& InChatMessageString)
+{
+	if (GEngine) GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Red, *ChatMessageString);
+}
--- Source/OnlineNumberBaseBall/Player/ONBPlayerController.h
+++ Source/OnlineNumberBaseBall/Player/ONBPlayerController.h
@@ -6,6 +6,7 @@
 #include "GameFramework/PlayerController.h"
 #include "ONBPlayerController.generated.h"
 
+class UONBChatInput;
 
 /**
  * 
@@ -14,5 +15,21 @@ UCLASS()
 class ONLINENUMBERBASEBALL_API AONBPlayerController : public APlayerController
 {
 	GENERATED_BODY()
+	
+public:
+	virtual void BeginPlay() override;
+	
+	void SetChatMessageString(const FString& IntChatMessageString);
+	
+	void PrintChatMessageString(const FString& InChatMessageString);
+	
+protected:
+	UPROPERTY(EditDefaultsOnly)
+	TSubclassOf<UONBChatInput> ChatInputWidgetClass;
+	
+	UPROPERTY()
+	TObjectPtr<UONBChatInput> ChatInputWidgetInstance;
+	
+	FString ChatMessageString;
 	
 };
